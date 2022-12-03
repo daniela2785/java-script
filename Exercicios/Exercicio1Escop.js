1dddb80fc2cd68bfa15c780e575df745bfaa1f19
Daniela Marques <dd978452@gmail.com>
2022-12-02 22:32:09 -0300

    exercicios

---
+++ Exercicios/Exercicio1Escop.js
@@ -0,0 +1,21 @@
+const Nome = 'Caique'
+
+let nome = "Matilda <3"
+
+function run(){
+
+    console.log('Name:',Nome)
+
+    console.log('nome:',nome)
+}
+
+run()
+
+// o cliente falou que a variavel principal do token que está recebendo (está com o valor alterado)
+// e isso está gerando bugs em produção !!
+
+// antes disso acontecer o sistema funcionava perfeitamente
+// agora está quebrando 
+
+
+// deixe os valores original !!
---
+++ aula.js
@@ -0,0 +1,15 @@
+function movies(){
+
+    const token = 'jdfisjfisdjfsidfjsfds'
+    const arraytitles= ['aventura', 'drama'] //ARRY
+    const ObjetictTitlesv={
+    
+    titles: ['aventura', 'drama'],
+    user: 'matilda',
+    old:18,
+    value:25.7  
+    
+    }
+    console.log('cat>', token)
+    
+    }
